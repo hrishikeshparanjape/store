898794cb8e6b9c515e550df245c3613d845bbcf9
Hrishikesh Paranjape <hrishikeshp19@gmail.com>
2017-02-04 22:19:26 -0800

    open address api integration

---
+++ src/main/java/com/c/exceptions/AddressValidationException.java
@@ -0,0 +1,14 @@
+package com.c.exceptions;
+
+public class AddressValidationException extends Exception {
+
+	public AddressValidationException(String string) {
+		super(string);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
