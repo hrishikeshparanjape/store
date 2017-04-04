0c99017920e20158ea00a8a24f62086a60f29c6c
Hrishikesh Paranjape <hrishikeshp19@gmail.com>
2017-04-03 20:33:14 -0700

    search by code

--- src/main/java/com/c/repositories/ZipCodeRepository.java
+++ src/main/java/com/c/repositories/ZipCodeRepository.java
@@ -1,9 +1,10 @@
 package com.c.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.c.domain.location.ZipCode;
 
 public interface ZipCodeRepository extends CrudRepository<ZipCode, Long> {
-	public ZipCode findByCode(String code);
+	public ZipCode findByCode(@Param("code") String code);
 }
